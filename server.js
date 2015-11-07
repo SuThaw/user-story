52b238d1caaa80e1f1663c4e65e4f4d153d2f14f
SuThaw <suthaw524@gmail.com>
2015-11-07 07:36:03 +0630

    added index and view

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan());
 
+app.get('*',function(req,res,next){
+	return res.sendFile(__dirname + '/public/views/index.html');
+});
+
 app.listen(config.port,function(){
 	console.log('server is listening at ' + '3000');
 });

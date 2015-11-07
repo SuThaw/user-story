e18333b5d2574bc8f11262fa3dc5d4f22201a2fe
SuThaw <suthaw524@gmail.com>
2015-11-07 07:28:22 +0630

    added config and apply some middleware

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var config = require('./config');
 
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan());
 
-app.listen(3000,function(){
+app.listen(config.port,function(){
 	console.log('server is listening at ' + '3000');
 });
